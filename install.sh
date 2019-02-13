c4a0dcb93f6808c443ba9ef25bcd581d390e2121
Alexander Nicholi <me@nicholatian.com>
2019-02-12 21:29:57 -0500

    allow custom install directory, and fix directory copies

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 #
 
+if [ "$1" = '' ]; then
+	dest="${HOME}";
+else 
+	dest="$1";
+fi
+
+echo "destination: ${dest}";
+
 for file in $(ls -1 src); do
-	cp -r "src/${file}" "${HOME}/.${file}";
+	if [ -d "${dest}/.${file}" ]; then
+		rm -rf "${dest}/.${file}";
+	fi
+	cp -r "src/${file}" "${dest}/.${file}";
 done
+
+source "${dest}/.bash_profile"
+
 unset file;
 unset bfile;
-
-source "${HOME}/.bash_profile"
+unset dest;
